9f42140ca3595736787e46347b2fb2e9c8e3acb7
Nagy Levente <nagylevente.504@gmail.com>
2025-07-01 15:57:37 +0200

    Useless lines has been deleted from HomeController

--- obj/Debug/net8.0/PalermosPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/PalermosPizza.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PalermosPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8dbc6d738d458a802242f80f1e6542b143f6f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PalermosPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PalermosPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
